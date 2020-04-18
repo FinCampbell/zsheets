dee5566e12fecbef5126816d1cc7be89a7013f57
Finlay Campbell <ec18069@qmul.ac.uk>
2020-04-18 01:18:33 +0000

    Removed old folders

--- timesheet/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from timesheet.views import BasicView, LoginView, RegisterView
-
-urlpatterns = [
-  path("", LoginView.as_view(), name="login"),
-  path("register/", RegisterView.as_view(), name="register"),
-  path("<pk>/", BasicView.as_view(), name="employee"),
-]
